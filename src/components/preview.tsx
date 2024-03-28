c0d0532a057a53f4889ca5565bf130b2c813dced
yair2000 <32851854+yair2000@users.noreply.github.com>
2024-03-28 17:30:51 +0200

    Solved a preview error
    Sometimes when a code is written, the following error appeared:
    Cannot read properties of null (reading 'contentWindow')
    
    Now this issue is resolved

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -11,54 +11,57 @@ interface PreviewProps{
   cell: Cell;
 }
 
-const html = `
-  <html>
-    <head>
-      <style>html { background-color: white; }</style>
-    </head>
-      <body>
-        <div id="root"/>
-          <script>
-            const errorHandler = (err) =>{
-              const root = document.querySelector('#root');
-              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-              console.error(err);
-            }
-            window.addEventListener('error', (event) =>{
-              event.preventDefault();
-              errorHandler(event.error)
-            })
-            window.addEventListener('message', (event) =>{
-            try{
-              // Output of the code in the child iframe
-              eval(event.data);
-            }
-            catch(err){
-              errorHandler(err);
-            }
-          }, false);
-          </script>
-      </body>
-  </html>
-`;
-
 const Preview: React.FC<PreviewProps> = ({ code, err, cell }) =>{
-  const iframe = useRef<any>();
+  const iframe = useRef<HTMLIFrameElement>(null);
 
   useEffect(() =>{
-    iframe.current.srcdoc = html;
-    setTimeout(() =>{
-      iframe.current.contentWindow.postMessage(code, '*'); // Posts the result from the code bundling
-    }, 10);
+    const setupIframe = () =>{
+      if(iframe.current){
+        const html = `
+          <html>
+            <head></head>
+            <body>
+              <div id="root"></div>
+              <script>
+                const handleError = (error) => {
+                  const root = document.querySelector('#root');
+                  root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + error + '</div>';
+                  console.error(error);
+                };
+
+                window.addEventListener('error', (event) => {
+                  event.preventDefault();
+                  handleError(event.error);
+                });
+
+                window.addEventListener('message', (event) => {
+                  try {
+                    eval(event.data);
+                  } catch (error) {
+                    handleError(error);
+                  }
+                }, false);
+              </script>
+            </body>
+          </html>
+        `;
+        iframe.current.onload = () =>{
+          // Prints the output of the code in the iframe element
+          iframe.current!.contentWindow!.postMessage(code, '*');
+        };
+
+        iframe.current.srcdoc = html;
+      }
+    }
+    setupIframe();
   }, [code]);
 
   return(
     <div className="preview-wrapper">
       <iframe
-      title="code preview"
       ref={iframe}
-      sandbox="allow-scripts"
-      srcDoc={html}/>
+      title="preview"
+      sandbox="allow-scripts"/>
       {err && (
         <div className="preview-error">
           {err}
